59ae769fa1c1865a97f7849872bf851aded15851
Souvik48 <iamsouvikdas48@gmail.com>
2021-05-27 19:58:11 +0530

    practice dummy api calls

--- app.js
+++ app.js
@@ -11,11 +11,18 @@ var btnTranslate = document.querySelector("#btn-translate");
 
 var txtInput = document.querySelector("#txt-input");
 
+var outputDiv = document.querySelector("#output");
 
+//outputDiv.innerText = "Souvik Das" 
+//if you want to write value then use id.innerText
+
+//if you want to read value then use id.value
 
 btnTranslate.addEventListener("click",function clickEventHandler(){
-    console.log("Clicked!");
-    console.log("input",txtInput.value)
+    //console.log("Clicked!");
+   //    console.log("input",txtInput.value)
+   outputDiv.innerText = "sdsaddaa " + txtInput.value 
+
 })
 
 
